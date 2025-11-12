8d27b824c6ef98ffa9c497fff0ca555164bc4130
Jiacheng <artin@cat.ms>
2025-11-12 16:56:45 +0000

    build: generate manifest.json

--- rna.config.mjs
+++ rna.config.mjs
@@ -1,3 +1,6 @@
+import manifestPlugin from 'esbuild-plugin-manifest';
+import { createHash } from 'crypto';
+
 export default {
   entrypoints: [
     {
@@ -10,4 +13,26 @@ export default {
   ],
   assetNames: 'assets/[name]-[hash]',
   chunkNames: '[ext]/[name]-[hash]',
+  plugins: [
+    manifestPlugin({
+      filename: 'manifest.json',
+      append: true,
+      // The `entries` object is what the contents of the manifest would normally be without using a custom `generate` function.
+      // It is a string to string mapping of the original asset name to the output file name.
+      generate: (entries) => {
+        const manifest = {};
+
+        for (const [source, file] of Object.entries(entries)) {
+          const realFile = typeof file === 'string' ? file : file.file;
+
+          manifest[source] = {
+            file: realFile,
+            hash: createHash('md5').update(realFile).digest('hex'),
+          };
+        }
+
+        return manifest;
+      },
+    }),
+  ],
 };
